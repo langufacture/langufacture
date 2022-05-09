0c01eec8aa17980ec3448f6911c8228da22817bc
langufacture <admin@langufacture.com>
2022-05-08 18:46:26 -0700

    Add favicon and logo to header

---
+++ src/scripts/main.js
@@ -0,0 +1,12 @@
+logoSetup = () => {
+    const img = document.createElement("img");
+    img.src = "/src/assets/langufacture.svg";
+    img.alt = "Langufacture logo (borromean trefoil)";
+    img.id = "logo";
+    document.body.appendChild(img);
+    console.log("setup complete");
+    const title = document.querySelector(".title");
+    title.insertBefore(img, title.firstChild);
+}
+window.addEventListener("load", logoSetup);
+console.log("loaded");
